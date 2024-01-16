1b38aa82412628e62d171a540618057bd8ba276a
Tenny27 <138973527+Tenny27@users.noreply.github.com>
2024-01-16 11:33:46 +0530

    Update simulation_program_2.py

--- simulation_program_2.py
+++ simulation_program_2.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 
 
-def simulation_program_2(main_analyzer):
-    user_id = int(input("Enter user ID: "))
+def simulation_program_2(main_analyzer,user_id):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
 
     # Add a prompt for the status input
-    status = int(input(f"Enter status for user {user_id} at {timestamp}: "))
+    status = int(input(f"\nEnter status for user {user_id} at {timestamp}: "))
+    if(status==1):
+        main_analyzer.insert_record(user_id, timestamp, status)
 
-    main_analyzer.insert_record(user_id, timestamp, status)
+    elif(status==0):
+        main_analyzer.insert_record(user_id, timestamp, status)
+
+    elif(status==-1):
+        q=[]
+        q.append(user_id)
+        print(q)
+    else:
+         print("Wrong input")
